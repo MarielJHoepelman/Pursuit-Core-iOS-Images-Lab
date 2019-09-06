96a0e632cf5378e00ab3c25b915236d25b935783
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-09-06 18:14:57 -0400

    Creates ComicsFromJSON model.

---
+++ API-images-lab/API-images-lab/Models/ComicsModel.swift
@@ -0,0 +1,23 @@
+//
+//  ComicsModel.swift
+//  API-images-lab
+//
+//  Created by Mariel Hoepelman on 9/6/19.
+//  Copyright © 2019 Mariel Hoepelman. All rights reserved.
+//
+
+import Foundation
+
+struct ComicsFromJSON: Codable {
+    let month: String
+    let num: Int
+    let link: String
+    let year: String
+    let news: String
+    let safe_title: String
+    let transcript: String
+    let alt: String
+    let img: String
+    let tittle: String
+    let day: String
+}
