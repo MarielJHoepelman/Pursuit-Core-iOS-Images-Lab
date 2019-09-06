ed35bd03649f178391b4308c012e6954a2762974
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-09-06 18:00:02 -0400

    Creates main view controllers and view controllers files.

---
+++ API-images-lab/API-images-lab/Controllers/PokemonViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonViewController.swift
+//  API-images-lab
+//
+//  Created by Mariel Hoepelman on 9/6/19.
+//  Copyright © 2019 Mariel Hoepelman. All rights reserved.
+//
+
+import UIKit
+
+class PokemonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
